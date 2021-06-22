906559764b8fe963f1ed0c8fdf00945d47c3c57c
Daniel <sergio.bosco@uap.edu.ar>
2021-06-22 01:40:09 -0300

    fixed weather model

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,7 +1,7 @@
 class Weather < ApplicationRecord
     has_many :data
 
-    validates :celsisu_temperature, :humidity, :wind_speed, :average_rainfall, :insolation, presence: true
+    validates :celsius_temperature, :humidity, :wind_speed, :average_rainfall, :insolation, presence: true
 
     paginates_per 8
 end
